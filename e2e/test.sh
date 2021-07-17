a8e61081d942c1689ad432c6b81b0daa3868e7f3
hanabi1224 <harlowmoo@gmail.com>
2021-07-17 21:38:15 +0800

    fix

--- e2e/test.sh
+++ e2e/test.sh
@@ -3,5 +3,6 @@ rm -r vendor || echo ''
 mkdir -p vendor/github.com/wasmerio/wasmer-go
 cp -r ../wasmer vendor/github.com/wasmerio/wasmer-go
 GODEBUG=cgocheck=2 go build -o app
-ldd app
+ldd app || echo ''
+otool -L app || echo ''
 ./app
