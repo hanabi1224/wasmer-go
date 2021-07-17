59ad011d841f75761035f08aae19afa65b404504
hanabi1224 <harlowmoo@gmail.com>
2021-07-17 21:13:27 +0800

    static link linux-amd64

---
+++ e2e/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+rm -r vendor || echo ''
+mkdir -p vendor/github.com/wasmerio/wasmer-go
+cp -r ../wasmer vendor/github.com/wasmerio/wasmer-go
+GODEBUG=cgocheck=2 go build -o app
+ldd app
+./app
